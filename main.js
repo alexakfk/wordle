6611015482b1dc2c3d635e0decf39b2f49655049
Alexa Kafka <alexakfk@gmail.com>
2022-03-22 13:33:11 -1000

    Upload Files
    Uploaded files to GitHub

---
+++ main.js
@@ -0,0 +1,61 @@
+const tileDisplay = document.querySelector('.tileContainer')
+const keyboard = document.querySelector('.keyContainer')
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '«',
+]
+
+const grid = [
+    '','','','','',
+    '','','','','',
+    '','','','','',
+    '','','','','',
+    '','','','','',
+    '','','','','',
+]
+
+grid.forEach(grid => {
+    const gridElement = document.createElement('div')
+    gridElement.setAttribute('id', grid)
+    tileDisplay.append(gridElement)
+})
+
+const handleClick = (e) => {
+    console.log(e.target)
+}
+
+keys.forEach(key => {
+    const buttonElement = document.createElement('button')
+    buttonElement.textContent = key
+    buttonElement.setAttribute('id', key)
+    buttonElement.addEventListener('click',handleClick)
+    keyboard.append(buttonElement)
+    
+})
